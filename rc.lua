f5f6eba7401e24708a72daea79a7225aab47dfb8
Sakyce <55030860+Sakyce@users.noreply.github.com>
2022-07-02 16:53:53 +0200

    I hope no one see this

--- rc.lua
+++ rc.lua
@@ -21,7 +21,25 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
 -- Error handling
-require("error_handler")
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
+do
+    local in_error = false
+    awesome.connect_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = tostring(err) })
+        in_error = false
+    end)
+end
 
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
@@ -31,12 +49,18 @@ editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 super = "Mod4"
 
+-- Update the repository & restart awesome
+local function restart_awesome()
+    awful.spawn("cd .config/awesome && gh repo sync") -- update my thing 
+    awesome.restart() 
+end
+
 -- launcher
 myawesomemenu = {
     { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
     { "manual", terminal .. "  man awesome" },
     { "edit config", editor_cmd .. " " .. awesome.conffile },
-    { "restart", awesome.restart },
+    { "restart", restart_awesome},
     { "quit", function() awesome.quit() end },
  }
 mymainmenu = awful.menu({ 
@@ -50,3 +74,32 @@ menubar.utils = terminal
 mykeyboardlayout = awful.widget.keyboardlayout()
 
 awful.rules.rules = require("liminal_rules") -- Set window rules
+
+-- Keybinds
+globalkeys = gears.table.join(
+    awful.key(
+        { super, }, "Return", function () awful.spawn(terminal) end,
+        {description = "open a terminal", group = "launcher"}
+    ),
+    awful.key( -- Restart the window manager
+        { super, }, "r", restart_awesome,
+        {description = "re", group = "window manager"}
+    )
+)
+
+-- Signal function to execute when a new client appears.
+client.connect_signal("manage", function (c)
+    if awesome.startup
+        and not c.size_hints.user_position
+        and not c.size_hints.program_position then
+        -- Prevent clients from being unreachable after screen count changes.
+        awful.placement.no_offscreen(c)
+    end
+end)
+    
+-- FOCUS
+client.connect_signal("mouse::enter", function(c)
+    c:emit_signal("request::activate", "mouse_enter", {raise = false})
+end)
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
--- update_awesome.sh
+++ update_awesome.sh
@@ -1,2 +1,8 @@
-cd liminal-awesome && gh repo sync
+# Clone repo
+sudo rm -r .config/awesome
+gh repo clone Sakyce/liminal-awesome .config/awesome
+
+# sync repo
+cd .config/awesome && gh repo sync
+
 cd .. && cp -r liminal-awesome ~/.config/awesome 
